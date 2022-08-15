2823e44630bec7f45ceef24050498ac6ad0c1c2b
asamahy <amrashraf@protonmail.com>
2022-08-15 09:26:36 +0200

    Modified the icon routine and silenced PlistBuddy
    Signed-off-by: asamahy <amrashraf@protonmail.com>

--- gfn-to-app.sh
+++ gfn-to-app.sh
@@ -2,12 +2,19 @@
 # resource_dasm from
 # https://github.com/fuzziqersoftware/resource_dasm
 # set -x
-if [[ -n "${1}" ]]; then
-    GAMEPATH="$1"
+if [[ -n "${1}" ]];
+    then
+        GAMEPATH="$1"
     else
-    echo Enter Game Shortcut path: 
-    read GAMEPATH
+        echo Enter Game Shortcut path: 
+        read GAMEPATH
 fi
+
+if [[ -n "$2" ]]; then
+echo image empty
+        APPICON="$2"
+fi
+
 
 # in="$1"
 inputFile="$(cat "$GAMEPATH")"
@@ -17,13 +24,10 @@ inputFile="$(cat "$GAMEPATH")"
 # x=${cmsID#*=}
 # echo $x
 gameID="${"${inputFile%%&*}"#*=}";
-# echo $gameID
-# echo
+
 fileName="${GAMEPATH##*/}"
 gameName="${fileName% on*}"
-# echo filename: $fileName
-# echo gameName: $gameName
-read ok
+
 tempDIR=$(mktemp -d)
 
 mkdir -p $tempDIR/"${gameName}".app
@@ -38,30 +42,29 @@ MACOSDIR=""${CONTENTSDIR}"/MacOS"
 mkdir "${CONTENTSDIR}"/Resources
 RESOURCESDIR=""${CONTENTSDIR}"/Resources"
 
-# echo made app
-# read ok
-
-echo "if you have a game icon put the location here (.icns file): "
-echo "if left empty then the icon from the shortcut will be used. \v"
-read APPICON
-
-# get the icon from original shortcut (method 1) found here: https://stackoverflow.com/q/73354927/11709309
+# getting the icon from original shortcut (method 1) found here: https://stackoverflow.com/q/73354927/11709309
 # icns="$(xattr -px com.apple.ResourceFork "$GAMEPATH")" # grab the resource fork from the input file in hex format
 # icns=${icns#*69 63 6E 73}   # using variable expansion delete the first 260 bytes including the magic number for icns.
 # icns=$(echo "69 63 6E 73$icns") # add the magic number back
 #
-# get the icon from original shortcut (method 2)
-set -x
-if [ -z $APPICON ];
-    then      
-        resource_dasm --target-type=icns "$GAMEPATH" $tempDIR/"${gameName}"-output
-        cp "$tempDIR/"${gameName}"-output"/*.icns "$RESOURCESDIR"/GameIcon.icns
+# getting the icon from original shortcut (method 2)
+if [ -z "$APPICON" ];
+    then
+        echo "\vif you have a game icon put the location here (.icns file): "
+        echo "if left empty then the icon from the shortcut will be used. \v"
+        read APPICON
+        if [ -z "$APPICON" ]; then      
+                resource_dasm --target-type=icns "$GAMEPATH" $tempDIR/"${gameName}"-output 2>/dev/null 1>&2
+                cp "$tempDIR/"${gameName}"-output"/*.icns "$RESOURCESDIR"/GameIcon.icns
+            else
+                cp "$APPICON" "$RESOURCESDIR"/GameIcon.icns
+        fi        
     else
         cp "$APPICON" "$RESOURCESDIR"/GameIcon.icns
 fi
-# read ok
+
 # create Info.plist
-/usr/libexec/PlistBuddy -c 'Add :CFBundleDevelopmentRegion string English' "$CONTENTSDIR"/Info.plist
+/usr/libexec/PlistBuddy -c 'Add :CFBundleDevelopmentRegion string English' "$CONTENTSDIR"/Info.plist 1>/dev/null
 /usr/libexec/PlistBuddy -c 'Add :CFBundleIconFile string GameIcon.icns' "$CONTENTSDIR"/Info.plist
 /usr/libexec/PlistBuddy -c 'Add :CFBundleInfoDictionaryVersion string 6.0' "$CONTENTSDIR"/Info.plist
 /usr/libexec/PlistBuddy -c 'Add :CFBundlePackageType string APPL' "$CONTENTSDIR"/Info.plist
@@ -70,10 +73,6 @@ fi
 /usr/libexec/PlistBuddy -c 'Add :NSPrincipalClass string NSApplication' "$CONTENTSDIR"/Info.plist
 /usr/libexec/PlistBuddy -c 'Add :CFBundleExecutable string GFN' "$CONTENTSDIR"/Info.plist
 /usr/libexec/PlistBuddy -c 'Add :LSUIElement bool true' "$CONTENTSDIR"/Info.plist
-
-# echo made plist
-# read ok
-
 
 # create the Executable file
 cat << ENDOFSCRIPT > "$CONTENTSDIR"/MacOS/GFN
@@ -91,8 +90,8 @@ if [ -f "/Applications/"$APPBUNDLE"" ]; then
         echo "File \""$APPBUNDLE"\" already exists"
         exit 1
     else
-        mv "$APPBUNDLE"/ /Applications/Games
+        mv "$APPBUNDLE" /Applications/Games
 fi
 
 
-echo Done
+echo "Done \a"
